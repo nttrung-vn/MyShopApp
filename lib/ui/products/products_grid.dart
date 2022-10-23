2a8bb08175f09324e8391b0cfd58929e6dc8ccdb
nttrung <nttrung.me@gmail.com>
2022-10-23 17:39:36 +0700

    Quan ly trang thai voi provider - ProductsManager

--- lib/ui/products/products_grid.dart
+++ lib/ui/products/products_grid.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
 import 'product_grid_tile.dart';
 
 import 'products_manager.dart';
+
+import '../../models/product.dart';
 
 class ProductsGrid extends StatelessWidget {
   final bool showFavorites;
@@ -11,9 +15,11 @@ class ProductsGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productsManager = ProductsManager();
-    final products =
-        showFavorites ? productsManager.favoriteItems : productsManager.items;
+    // Đọc ra danh sách các product sẽ được hiển thị từ ProductsManager
+    final products = context.select<ProductsManager, List<Product>>(
+        (productsManager) => showFavorites
+            ? productsManager.favoriteItems
+            : productsManager.items);
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
--- lib/ui/products/user_product_list_tile.dart
+++ lib/ui/products/user_product_list_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../../models/product.dart';
+import 'products_manager.dart';
 
 class UserProductListTile extends StatelessWidget {
   final Product product;
@@ -31,8 +33,18 @@ class UserProductListTile extends StatelessWidget {
   Widget buildDeleteButton(BuildContext context) {
     return IconButton(
       icon: const Icon(Icons.delete),
-      onPressed: () async {
-        print('Delete a product');
+      onPressed: () {
+        context.read<ProductsManager>().deleteProduct(product.id!);
+        ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(
+            const SnackBar(
+              content: Text(
+                'Product deleted',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
       },
       color: Theme.of(context).errorColor,
     );
