4f7496e16127fcb596b0c3f2bdf9c1dfab680599
nttrung <nttrung.me@gmail.com>
2022-10-30 05:21:36 +0700

    Cap nhat, xoa san pham tren Realtime Database cua Firebase

--- lib/ui/products/products_manager.dart
+++ lib/ui/products/products_manager.dart
@@ -45,31 +45,30 @@ class ProductsManager with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  // void addProduct(Product product) {
-  //   _items.add(
-  //     product.copyWith(
-  //       id: 'p${DateTime.now().toIso8601String()}',
-  //     ),
-  //   );
-  //   notifyListeners();
-  // }
-
-  void updateProduct(Product product) {
-    final index = _items.indexWhere((item) => item.id == product.id);
-    if (index >= 0) {
-      _items[index] = product;
-      notifyListeners();
-    }
-  }
-
   void toggleFavoriteStatus(Product product) {
     final savedStatus = product.isFavorite;
     product.isFavorite = !savedStatus;
   }
 
-  void deleteProduct(String id) {
+  Future<void> updateProduct(Product product) async {
+    final index = _items.indexWhere((item) => item.id == product.id);
+    if (index >= 0) {
+      if (await _productsService.updateProduct(product)) {
+        _items[index] = product;
+        notifyListeners();
+      }
+    }
+  }
+
+  Future<void> deleteProduct(String id) async {
     final index = _items.indexWhere((item) => item.id == id);
+    Product? existingProduct = _items[index];
     _items.removeAt(index);
     notifyListeners();
+
+    if (!await _productsService.deleteProduct(id)) {
+      _items.insert(index, existingProduct);
+      notifyListeners();
+    }
   }
 }
